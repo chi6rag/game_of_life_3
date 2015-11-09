cd95359d9c329995676bd213ef1c27381e4963f8
Chirag Aggarwal <me@chi6rag.net>
2015-11-09 14:25:46 +0530

    Refactoring Cell

--- lib/game_of_life/cell.rb
+++ lib/game_of_life/cell.rb
@@ -18,8 +18,25 @@ module GameOfLife
 
     def ==(other)
       return false unless other.is_a?(Cell)
-      return true if((self.x == other.x) && (self.y == other.y) && (self.is_alive? == other.is_alive?))
+      return true if (has_same_x_coordinates?(other) &&
+                      has_same_y_coordinates?(other) &&
+                      has_same_alive_status?(other)
+      )
       false
     end
+
+    private
+    def has_same_x_coordinates?(other)
+      self.x == other.x
+    end
+
+    def has_same_y_coordinates?(other)
+      self.y == other.y
+    end
+
+    def has_same_alive_status?(other)
+      self.is_alive? == other.is_alive?
+    end
+
   end
 end
